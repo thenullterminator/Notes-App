78790ebabbf08d3f8a1ec99849e44089af5dd8a0
thenullterminator <dazz2803@gmail.com>
2019-05-17 22:32:09 +0530

    Add function

--- commands.js
+++ commands.js
@@ -1,3 +1,25 @@
+const fs=require("fs");
+
+
 var Add=(title,body)=>{
 
+    var currentnote={
+        title:title,
+        body:body
+    };
+
+    var allnotes=[];
+    try
+    {
+        allnotes=JSON.parse(fs.readFileSync("./notes-data.json"));
+    }
+    catch(e){}
+
+    var dupnotes=allnotes.filter((currentnote)=>currentnote.title===title);
+
+    if(dupnotes.length===0)
+    {
+        allnotes.push(currentnote);
+        fs.writeFileSync("./notes-data.json",JSON.stringify(allnotes));
+    }
 };
